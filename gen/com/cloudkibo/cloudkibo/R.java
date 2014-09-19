009f3aa5951eb8995de0ab8d1f20fa60ad1a0eb2
sabachanna <shabir.saba@gmail.com>
2014-09-19 08:51:49 -0400

    androids UI updated

--- gen/com/cloudkibo/cloudkibo/R.java
+++ gen/com/cloudkibo/cloudkibo/R.java
@@ -11,12 +11,49 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int fb=0x7f020001;
+        public static final int ggle=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int icon=0x7f020004;
+    }
+    public static final class id {
+        public static final int RelativeLayout1=0x7f060000;
+        public static final int Space1=0x7f06000b;
+        public static final int TextView01=0x7f060006;
+        public static final int button1=0x7f060001;
+        public static final int editText1=0x7f060002;
+        public static final int editText2=0x7f060004;
+        public static final int editText3=0x7f06000d;
+        public static final int editText5=0x7f06000e;
+        public static final int editText6=0x7f060010;
+        public static final int imageButton1=0x7f060008;
+        public static final int imageButton2=0x7f060009;
+        public static final int link_to_register=0x7f060007;
+        public static final int textView1=0x7f060003;
+        public static final int textView2=0x7f060005;
+        public static final int textView3=0x7f06000a;
+        public static final int textView4=0x7f06000c;
+        public static final int textView5=0x7f06000f;
+    }
+    public static final class layout {
+        public static final int home=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int register=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
-        public static final int host=0x7f030001;
-        public static final int port=0x7f030002;
+        public static final int app_name=0x7f040000;
+        public static final int email=0x7f040005;
+        public static final int firstName=0x7f040008;
+        public static final int forgotPassword=0x7f04000b;
+        public static final int host=0x7f040001;
+        public static final int lastName=0x7f04000a;
+        public static final int password=0x7f040009;
+        public static final int password_txt=0x7f040004;
+        public static final int phone_no=0x7f040006;
+        public static final int port=0x7f040002;
+        public static final int username=0x7f040007;
+        public static final int username_txt=0x7f040003;
     }
     public static final class style {
         /** 
@@ -40,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
