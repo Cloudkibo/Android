de5a50330bdec78959ca278efe9dee32f48ef4db
sabachanna <shabir.saba@gmail.com>
2014-09-20 12:19:14 -0400

    Login

--- gen/com/cloudkibo/cloudkibo/R.java
+++ gen/com/cloudkibo/cloudkibo/R.java
@@ -19,22 +19,25 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f060000;
-        public static final int Space1=0x7f06000b;
-        public static final int TextView01=0x7f060006;
-        public static final int button1=0x7f060001;
-        public static final int editText1=0x7f060002;
-        public static final int editText2=0x7f060004;
-        public static final int editText3=0x7f06000d;
-        public static final int editText5=0x7f06000e;
-        public static final int editText6=0x7f060010;
-        public static final int imageButton1=0x7f060008;
-        public static final int imageButton2=0x7f060009;
-        public static final int link_to_register=0x7f060007;
-        public static final int textView1=0x7f060003;
-        public static final int textView2=0x7f060005;
-        public static final int textView3=0x7f06000a;
-        public static final int textView4=0x7f06000c;
-        public static final int textView5=0x7f06000f;
+        public static final int Space1=0x7f06000e;
+        public static final int TextView01=0x7f06000c;
+        public static final int callBtn=0x7f060003;
+        public static final int editText3=0x7f060010;
+        public static final int editText5=0x7f060011;
+        public static final int editText6=0x7f060013;
+        public static final int forgotPass=0x7f06000b;
+        public static final int imageButton1=0x7f060009;
+        public static final int imageButton2=0x7f06000a;
+        public static final int loginBtn=0x7f060008;
+        public static final int logout=0x7f060004;
+        public static final int passwordText=0x7f060006;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060007;
+        public static final int textView3=0x7f06000d;
+        public static final int textView4=0x7f06000f;
+        public static final int textView5=0x7f060012;
+        public static final int textwelcome=0x7f060005;
+        public static final int userNameText=0x7f060001;
     }
     public static final class layout {
         public static final int home=0x7f030000;
---
+++ src/com/cloudkibo/cloudkibo/Login.java
@@ -0,0 +1,245 @@
+package com.cloudkibo.cloudkibo;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.cloudkibo.library.DatabaseHandler;
+import com.cloudkibo.library.UserFunctions;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cloudkibo.library.DatabaseHandler;
+import com.cloudkibo.library.UserFunctions;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Login extends Activity {
+	
+	Button loginBtn;
+    Button Btnregister;
+    Button passreset;
+    EditText userNameText;
+    EditText passwordText;
+    
+    /**
+     * Called when the activity is first created.
+     */
+    private static String KEY_SUCCESS = "status";
+    private static String KEY_UID = "_id";
+    private static String KEY_USERNAME = "username";
+    private static String KEY_FIRSTNAME = "firstname";
+    private static String KEY_LASTNAME = "lastname";
+    private static String KEY_EMAIL = "email";
+    private static String KEY_CREATED_AT = "date";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login);
+        
+        userNameText = (EditText) findViewById(R.id.userNameText);
+        passwordText = (EditText) findViewById(R.id.passwordText);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        
+        //Btnregister = (Button) findViewById(R.id.registerbtn);
+        //passreset = (Button)findViewById(R.id.passres);
+        
+        /**
+         * Login button click event
+         * A Toast is set to alert when the Email and Password field is empty
+         **/
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                if (  ( !userNameText.getText().toString().equals("")) && ( !passwordText.getText().toString().equals("")) )
+                {
+                    NetAsync(view);
+                }
+                else if ( ( !userNameText.getText().toString().equals("")) )
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "Username is required", Toast.LENGTH_SHORT).show();
+                }
+                else if ( ( !passwordText.getText().toString().equals("")) )
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "Password is required", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),
+                            "Username and Password are required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void NetAsync(View view){
+        new NetCheck().execute();
+    }
+    
+    /**
+     * Async Task to check whether internet connection is working.
+     **/
+
+    private class NetCheck extends AsyncTask<String,String,Boolean>
+    {
+        private ProgressDialog nDialog;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            nDialog = new ProgressDialog(Login.this);
+            nDialog.setTitle("Checking Network");
+            nDialog.setMessage("Loading..");
+            nDialog.setIndeterminate(false);
+            nDialog.setCancelable(true);
+            //nDialog.show();
+        }
+        /**
+         * Gets current device state and checks for working internet connection by trying Google.
+        **/
+        @Override
+        protected Boolean doInBackground(String... args){
+
+
+
+            ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.isConnected()) {
+                try {
+                    URL url = new URL("http://www.google.com");
+                    HttpURLConnection urlc = (HttpURLConnection) url.openConnection();
+                    urlc.setConnectTimeout(3000);
+                    urlc.connect();
+                    if (urlc.getResponseCode() == 200) {
+                        return true;
+                    }
+                } catch (MalformedURLException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return false;
+
+        }
+        @Override
+        protected void onPostExecute(Boolean th){
+
+            if(th == true){
+                nDialog.dismiss();
+                new ProcessLogin().execute();
+            }
+            else{
+                nDialog.dismiss();
+                Toast.makeText(getApplicationContext(),
+                        "Could not connect to Internet", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    
+    /**
+     * Async Task to get and send data to My Sql database through JSON respone.
+     **/
+    private class ProcessLogin extends AsyncTask<String, String, JSONObject> {
+
+
+        private ProgressDialog pDialog;
+
+        String username, password;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            userNameText = (EditText) findViewById(R.id.userNameText);
+            passwordText = (EditText) findViewById(R.id.passwordText);
+            username = userNameText.getText().toString();
+            password = passwordText.getText().toString();
+            pDialog = new ProgressDialog(Login.this);
+            pDialog.setTitle("Contacting CloudKibo");
+            pDialog.setMessage("Logging in ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... args) {
+
+            UserFunctions userFunction = new UserFunctions();
+            JSONObject json = userFunction.loginUser(username, password);
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+            	
+               if (json.getString(KEY_SUCCESS) != null) {
+
+                    String res = json.getString(KEY_SUCCESS);
+
+                    if(res.equals("success")){
+                        pDialog.setMessage("Loading User Space");
+                        pDialog.setTitle("Getting Data");
+                        DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+                        JSONObject json_user = json.getJSONObject("user");
+                        /**
+                         * Clear all previous data in SQlite database.
+                         **/
+                        UserFunctions logout = new UserFunctions();
+                        logout.logoutUser(getApplicationContext());
+                        db.addUser(json_user.getString(KEY_FIRSTNAME),json_user.getString(KEY_LASTNAME),json_user.getString(KEY_EMAIL),json_user.getString(KEY_USERNAME),json_user.getString(KEY_UID),json_user.getString(KEY_CREATED_AT));
+                       /**
+                        *If JSON array details are stored in SQlite it launches the User Panel.
+                        **/
+                        
+                        Intent upanel = new Intent(getApplicationContext(), Home.class);
+                        upanel.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        pDialog.dismiss();
+                        startActivity(upanel);
+                        
+                        /**
+                         * Close Login Screen
+                         **/
+                        finish();
+                    }
+                    else{
+
+                        pDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),
+                                "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+       }
+    }
+    
+
+    
+}
