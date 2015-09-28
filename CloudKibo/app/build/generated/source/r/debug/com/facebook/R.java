65a684e60462560561bace28381f1e0d84dc9683
sojharo <sojharo@gmail.com>
2015-09-29 02:02:48 +0500

    invite contacts to cloudkibo from address book UI 37

--- CloudKibo/app/build/generated/source/r/debug/com/facebook/R.java
+++ CloudKibo/app/build/generated/source/r/debug/com/facebook/R.java
@@ -114,12 +114,12 @@ public final class R {
 		public static final int com_facebook_activity_layout = 0x7f030009;
 		public static final int com_facebook_login_fragment = 0x7f03000a;
 		public static final int com_facebook_tooltip_bubble = 0x7f03000b;
-		public static final int messenger_button_send_blue_large = 0x7f030017;
-		public static final int messenger_button_send_blue_round = 0x7f030018;
-		public static final int messenger_button_send_blue_small = 0x7f030019;
-		public static final int messenger_button_send_white_large = 0x7f03001a;
-		public static final int messenger_button_send_white_round = 0x7f03001b;
-		public static final int messenger_button_send_white_small = 0x7f03001c;
+		public static final int messenger_button_send_blue_large = 0x7f030018;
+		public static final int messenger_button_send_blue_round = 0x7f030019;
+		public static final int messenger_button_send_blue_small = 0x7f03001a;
+		public static final int messenger_button_send_white_large = 0x7f03001b;
+		public static final int messenger_button_send_white_round = 0x7f03001c;
+		public static final int messenger_button_send_white_small = 0x7f03001d;
 	}
 	public static final class string {
 		public static final int com_facebook_image_download_unknown_error = 0x7f06001d;
