04b4af9051cfdf65b52a8ac9be5aca28eeca1282
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2016-11-24 19:08:26 +0500

    UI Improved

--- app/src/main/java/com/cloudkibo/ui/GroupChat.java
+++ app/src/main/java/com/cloudkibo/ui/GroupChat.java
@@ -436,7 +436,9 @@ public class GroupChat extends CustomFragment implements IFragmentName
 						R.layout.chat_item_rcv, null);
 
 			TextView lbl = (TextView) v.findViewById(R.id.lblContactDisplayName);
-			lbl.setText(c.getDate().replaceAll("-", "/").split("/",2)[1]);
+			lbl.setText(c.getDate().replaceAll("-", "/").split("/",2)[1].split(" ")[1]);
+			TextView phone = (TextView) v.findViewById(R.id.phone);
+			phone.setVisibility(View.GONE);
 
 			lbl = (TextView) v.findViewById(R.id.lbl2);
 			lbl.setText(c.getMsg());
