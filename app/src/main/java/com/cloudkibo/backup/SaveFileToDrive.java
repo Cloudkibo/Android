85299c3ba0736fb4da59d228946a430f48c750f4
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2017-01-11 16:13:25 +0500

    file back up code and group archive message bug fix

---
+++ app/src/main/java/com/cloudkibo/backup/SaveFileToDrive.java
@@ -0,0 +1,138 @@
+package com.cloudkibo.backup;
+
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SaveFileToDrive extends BaseActivity {
+    private static final String TAG = "SaveFileToDrive";
+    private static final String MIME_PHOTO = "";
+    private static final String MIME_VIDEO = "";
+
+    private GoogleApiClient googleApiClient;
+
+
+    private void saveFiletoDrive(final File file, final String mime) {
+
+        Drive.DriveApi.newDriveContents(getGoogleApiClient()).setResultCallback(
+                new ResultCallback<DriveApi.DriveContentsResult>() {
+                    @Override
+                    public void onResult(DriveApi.DriveContentsResult result) {
+                        // If the operation was not successful, we cannot do
+                        // anything
+                        // and must
+                        // fail.
+                        if (!result.getStatus().isSuccess()) {
+                            Log.i(TAG, "Failed to create new contents.");
+                            return;
+                        }
+                        Log.i(TAG, "Connection successful, creating new contents...");
+                        // Otherwise, we can write our data to the new contents.
+                        // Get an output stream for the contents.
+                        OutputStream outputStream = result.getDriveContents()
+                                .getOutputStream();
+                        FileInputStream fis;
+                        try {
+                            fis = new FileInputStream(file.getPath());
+                            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                            byte[] buf = new byte[1024];
+                            int n;
+                            while (-1 != (n = fis.read(buf)))
+                                baos.write(buf, 0, n);
+                            byte[] photoBytes = baos.toByteArray();
+                            outputStream.write(photoBytes);
+
+                            outputStream.close();
+                            outputStream = null;
+                            fis.close();
+                            fis = null;
+
+                        } catch (FileNotFoundException e) {
+                            Log.w(TAG, "FileNotFoundException: " + e.getMessage());
+                        } catch (IOException e1) {
+                            Log.w(TAG, "Unable to write file contents." + e1.getMessage());
+                        }
+
+                        String title = file.getName();
+                        MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                                .setMimeType(mime).setTitle(title).build();
+
+                        if (mime.equals(MIME_PHOTO)) { //Please assign value to MIME_photo at top
+
+                                Log.i(TAG, "Creating new photo on Drive (" + title
+                                        + ")");
+                            Drive.DriveApi.getFolder(getGoogleApiClient(),
+                                    result.getDriveContents().getDriveId()).createFile(getGoogleApiClient(),
+                                    metadataChangeSet,
+                                    result.getDriveContents());
+                        } else if (mime.equals(MIME_VIDEO)) { //Please assign value to MIME_video at top
+                            Log.i(TAG, "Creating new video on Drive (" + title
+                                    + ")");
+                            Drive.DriveApi.getFolder(getGoogleApiClient(),
+                                    result.getDriveContents().getDriveId()).createFile(getGoogleApiClient(),
+                                    metadataChangeSet,
+                                    result.getDriveContents());
+                        }
+
+                    }
+                });
+    }
+        // Start by creating a new contents, and setting a callback.
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addScope(Drive.SCOPE_APPFOLDER)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Called whenever the API client fails to connect.
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+        if (!result.hasResolution()) {
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        // The failure has a resolution. Resolve it.
+        // Called typically when the app is not yet authorized, and an
+        // authorization
+        // dialog is displayed to the user.
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
+        }
+    }
+}
